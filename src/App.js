6e66880652604e6c78f8f889797789cb7b8647bd
Lunik <guillaume.lunik@gmail.com>
2017-06-04 19:36:32 +0200

    adding profile view

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
+import firebaseApp from './components/firebase/firebaseApp'
 import Login from './components/login'
+import Profile from './components/profile'
+import Friend from './components/friend'
 
-class App extends Component {
-  render() {
-    return (
-        <Login/>
-    );
-  }
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.props = props
+        this.state = {
+            user: firebaseApp.auth().currentUser,
+            hash: window.location.hash.slice(1)
+        }
+
+        window.onhashchange = () => this.updateHash()
+    }
+    updateHash(){
+        this.setState({
+            hash: window.location.hash.slice(1)
+        })
+    }
+    setUser(user){
+        this.setState({
+            user: user,
+        })
+    }
+    render() {
+        if(this.state.user) {
+            if(this.state.hash){
+                return (
+                   <Friend uid={this.state.hash} />
+                )
+            } else {
+                return (
+                    <Profile user={this.state.user}/>
+                )
+            }
+        } else {
+            return (
+                <Login setUser={(user) => this.setUser(user)}/>
+            )
+        }
+    }
 }
 
 export default App;
